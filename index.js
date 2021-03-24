4d830046cea1ff78522509b2e935e6403d114085
Danny <danny.costelloe@gmail.com>
2021-03-24 08:16:00 -0400

    commit tree 239276b28820a2003055e6d5c9f2e0965115d7dd

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(name, email){
+
+    const obj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+          name: `${name}`,
+          email: `${email}`
+      })
+    };
+  
+    return fetch("http://localhost:3000/users", obj)
+    .then(res => {
+      return res.json();
+    })
+    .then(function(obj2) {
+      document.body.innerHTML = obj2["id"];
+    })
+    .catch(function(err) {
+      document.body.innerHTML = err.message;
+    }); 
+  }
